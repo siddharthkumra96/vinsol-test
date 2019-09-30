4fcbd1546ac62a9a5304137047e8437c4590f158
siddharthkumra96 <siddharthkumra96@gmail.com>
2019-09-30 15:14:10 +0530

    Updates

---
+++ precache-manifest.df6e3174927e815fe9d94b95c3ed2ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29bff56baff0845cb278e4d78fa4136",
+    "url": "/vinsol-quiz/index.html"
+  },
+  {
+    "revision": "249d3f05c07c44e74c6b",
+    "url": "/vinsol-quiz/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "13283c987f9bde402322",
+    "url": "/vinsol-quiz/static/js/2.61e73b92.chunk.js"
+  },
+  {
+    "revision": "249d3f05c07c44e74c6b",
+    "url": "/vinsol-quiz/static/js/main.ff46a64f.chunk.js"
+  },
+  {
+    "revision": "1a19056bc0f32b3d1541",
+    "url": "/vinsol-quiz/static/js/runtime-main.b3ad5350.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vinsol-quiz/static/media/logo.25bf045c.svg"
+  }
+]);
